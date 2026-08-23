374b4838a7728d7b89df2cc6c88e6568ee3da3bc
Wildeys <wildhaan11@gmail.com>
2026-08-24 02:25:11 +0500

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-});
+  base: '/latin-mv-tlt/',
+})
