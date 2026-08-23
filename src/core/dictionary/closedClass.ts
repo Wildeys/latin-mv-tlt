f77982556a7e098ce14fe2bcea984646188b8979
wildeys <wildhaan11@gmail.com>
2026-08-24 02:33:48 +0500

    Remove 'dhaanan' translation from LATIN_TO_ENGLISH dictionary in closedClass.ts

--- src/core/dictionary/closedClass.ts
+++ src/core/dictionary/closedClass.ts
@@ -46,7 +46,6 @@ export const LATIN_TO_ENGLISH: Record<string, string> = {
   ann: 'come',
   male: 'Malé',
   maale: 'Malé',
-  dhaanan: 'go',
   miadhu: 'today',
   iyye: 'yesterday',
   fen: 'water',
